8c68ac829dfbf4b121a26e66cf41e8299c09814a
redebron20 <redebron20@gmail.com>
2021-06-30 00:20:57 -0400

    Set up model associations and validations.

--- budgetize_api/app/models/budget.rb
+++ budgetize_api/app/models/budget.rb
@@ -1,2 +1,5 @@
 class Budget < ApplicationRecord
+    belongs_to :user
+
+    validates :name, presence: true, uniqueness: true
 end
--- budgetize_api/app/models/user.rb
+++ budgetize_api/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_many :budgets
+
+    validates :email, presence: true, uniqueness: true
+    validates :username, presence: true, uniqueness: true
+
+    has_secure_password
 end
