084f48ddfc3c24c1298143e2c4d02771de51f6a4
redebron20 <redebron20@gmail.com>
2021-07-06 03:52:23 -0400

    Create funtionality to add new budget and show balance.

--- budgetize_client/javascripts/budget.js
+++ budgetize_client/javascripts/budget.js
@@ -2,7 +2,8 @@ class Budget{
 
     static all = []
     static budgetsContainer = document.getElementById("budgets-container")
-    static budgetForm = document.getElementById("budget-form-container")
+    static budgetForm = document.getElementById("budget-form")
+    static budgetFeedback = document.querySelector(".budget-feedback")
 
     constructor({id, name, income, expense, balance, user_id}){
         this.id = id
@@ -25,6 +26,7 @@ class Budget{
             <div>
                 <h3>${this.name}</h3>
             </div>
+            <button id="edit-bttn">Edit</button>
             <button id="delete-bttn">Delete</button>
         `
         return this.element
@@ -36,10 +38,15 @@ class Budget{
 
     static renderForm(){
         Budget.budgetForm.innerHTML += `
-        <form id="new-budget-form">
-            Create Budget: <input type="text" id="name">
-            <input type="submit" id="create">
-        </form>
+        <form id="budget-form" class=" budget-form">
+                 <h5 class="text-capitalize">create new budget</h5>
+                 <div class="form-group">
+                    Name: <input type="text" class="form-control budget-input" id="name">
+                    Amount: <input type="number" class="form-control budget-input" id="budget-input">
+                 </div>
+                 <!-- submit button -->
+                 <button type="submit" class="btn text-capitalize budget-submit" id="budget-submit">create</button>
+            </form>
         `
     }
 
--- budgetize_client/javascripts/index.js
+++ budgetize_client/javascripts/index.js
@@ -15,9 +15,69 @@ class UI {
       this.itemList = [];
       this.itemID = 0;
     }
+
+    submitBudgetForm(){
+        const value = this.budgetInput.value;
+        if(value==='' || value <0){
+            this.budgetFeedback.classList.add('showItem')
+            this.budgetFeedback.innerHTML = `<p>value cannot be negative or empty</p>`;
+            const self = this;
+            setTimeout(function(){
+                self.budgetFeedback.classList.remove('showItem')
+            }, 5000);
+        }
+        else{
+            this.budgetAmount.textContent = value;
+            this.budgetInput.value = '';
+            this.showBalance();
+        }
+    }
+
+    showBalance(){
+        const expense = this.totalExpense();
+        const total = parseInt(this.budgetAmount.textContent) - expense;
+        this.balanceAmount.textContent = total;
+    }
+
+    totalExpense(){
+        let total = 400;
+        return total
+    }
   }
+
+const base_url = "http://127.0.0.1:3000"
+const budgetService = new BudgetService(base_url)
+
+function addEventListeners(){
+    const budgetForm = document.getElementById('budget-form')
+    const expenseForm = document.getElementById('expense-form')
+    const expenseList = document.getElementById('expense-list')
+
+    const ui = new UI()
+
+    budgetForm.addEventListener('submit', function(event){
+        event.preventDefault();
+        ui.submitBudgetForm();
+
+    });
+
+    expenseForm.addEventListener('submit', function(event){
+
+    });
+
+    expenseList.addEventListener('click', function(event){
+
+    });
+
+
+}
 
 document.addEventListener('DOMContentLoaded', () =>{
     console.log("DOM HAS LOADED!!!");
+    addEventListeners();
     renderLogInForm()
+    // budgetService.getBudgets()
+    // Budget.renderForm()
+
 })
+
