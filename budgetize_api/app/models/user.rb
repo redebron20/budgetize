ae245bb55515c1c9f49aae7c09de862047786d23
redebron20 <redebron20@gmail.com>
2021-07-06 13:27:28 -0400

    Create class expense and function to add new expense item.

--- budgetize_api/app/models/user.rb
+++ budgetize_api/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :budgets
+    has_one :budget
 
     validates :email, presence: true, uniqueness: true
     validates :username, presence: true, uniqueness: true
--- budgetize_api/config/routes.rb
+++ budgetize_api/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :expenses
-  resources :transactions
-  resources :budgets
-  resources :users
+  resources :users do 
+    resources :budgets do
+      resources :expenses
+    end 
+  end 
+
+  post '/login', to: 'sessions#create'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
