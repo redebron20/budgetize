086633f2120d75721e7b37b9eaede5cb5b1676f9
redebron20 <redebron20@gmail.com>
2021-07-06 22:56:12 -0400

    Create fetch request to successfully signup and login users.

--- budgetize_api/app/controllers/sessions_controller.rb
+++ budgetize_api/app/controllers/sessions_controller.rb
@@ -1,11 +1,11 @@
 class SessionsController < ApplicationController 
     def create
         user = User.find_by(:email => params[:email])
-        
+
         if user && user.authenticate(params[:password])
             render :json => user, :only => [:id, :email]
         else 
-            render :json => { errors: 'Invalid email or password' }, status: :unauthorized
+            render :json => { errors: "Something went wrong. Please try again." }
         end 
     end 
 
